950c966397ec7bc84f02a6653dee7eccea308a1d
Jhordan Sax <71660993@continental.edu.pe>
2023-12-04 05:14:27 -0500

    Proyecto Algoritmo RSA

---
+++ main.py
@@ -0,0 +1,187 @@
+from package.utilidades import *
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog
+
+def gui_generar():
+    inicio.hide()
+    generar.show()
+    
+def gui_encriptar():
+    inicio.hide()
+    encriptar.show()
+    
+def gui_desencriptar():
+    inicio.hide()
+    desencriptar.show()
+    
+def  volver():
+    generar.hide()
+    encriptar.hide()
+    desencriptar.hide()
+    inicio.show()
+    
+# Eventos de GENERAR
+def btnGenerarPrimos() -> None:
+    bits = 16
+    
+    p = generar_primo(bits)
+    q = generar_primo(bits)
+    
+    generar.boxPrimer.setText(str(p))
+    generar.boxSegun.setText(str(q))
+    generar.boxExpo.setText("65537")
+    
+    limpiarDatosErrores()
+
+def btnGenerarPublica():
+    p, q, e = validacionesDeEntrada()
+    
+    if not p:
+        return
+    
+    limpiarDatosErrores()
+    
+    # Generar llave publica
+    llave_publica = generar_clave_publica(p, q, e)
+    
+    # Codificar llave en texto
+    publ = llave_a_texto_public(llave_publica)
+    
+    # Guardar llave en un archivo de texto
+    guardarArchivo("llave_publica", publ)
+    
+
+def btnGenerarPrivada():
+    p, q, e = validacionesDeEntrada()
+    
+    if not p:
+        return
+    
+    limpiarDatosErrores()
+    
+    # Generar llave privada
+    llave_privada = generar_clave_privada(p, q, e)
+    
+    # Codificar llave en texto
+    priv = llave_a_texto_private(llave_privada)
+    
+    # Guardar llave en un archivo de texto
+    guardarArchivo("llave_privada", priv)
+    
+def guardarArchivo(name: str, value: str):
+    archivo, _ = QFileDialog.getSaveFileName(None, "Guardar Llave...", 'C:\\')
+    
+    with open(archivo, 'wt') as f:
+        f.write(value)
+
+def limpiarDatosErrores() -> None:
+    generar.errorPrimo1.setText("")
+    generar.errorPrimo2.setText("")
+    generar.errorPrimo3.setText("")
+
+def validacionesDeEntrada() -> tuple:
+    boxPrimer = generar.boxPrimer.text()
+    boxSegun = generar.boxSegun.text()
+    boxExpo = generar.boxExpo.text()
+    
+    # Validando campos vacíos
+    if boxPrimer == "":
+        generar.errorPrimo1.setText("Rellenar este campo!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if boxSegun == "":
+        generar.errorPrimo2.setText("Rellenar este campo!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if boxExpo == "":
+        generar.errorPrimo3.setText("Rellenar este campo!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    # Validadndo si son números
+    if not es_numero(boxPrimer):
+        generar.errorPrimo1.setText("Este valor no es un número!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if not es_numero(boxSegun):
+        generar.errorPrimo2.setText("Este valor no es un número!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if not es_numero(boxExpo):
+        generar.errorPrimo3.setText("Este valor no es un número!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    # Validar si es primo
+    if not es_primo(int(boxPrimer)):
+        generar.errorPrimo1.setText("Este no es un número PRIMO!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if not es_primo(int(boxSegun)):
+        generar.errorPrimo2.setText("Este no es un número PRIMO!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    if not es_primo(int(boxExpo)):
+        generar.errorPrimo3.setText("Este no es un número PRIMO!!!")
+        return (False, False, False)
+    limpiarDatosErrores()
+    return (int(boxPrimer), int(boxSegun), int(boxExpo))
+
+def errorGenerarBox(number: int) -> str:
+    pass
+
+if __name__ == "__main__":
+    """ bits = 16  # Número pequeño para propósitos de demostración
+    # Genera claves RSA con primos de 'bits' bits
+    p = generar_primo(bits)
+    q = generar_primo(bits)
+    # Uso del código para generar claves
+    llave_publica = generar_clave_publica(p, q)
+    llave_privada = generar_clave_privada(p, q)
+
+    print("Llave Pública:", llave_publica)
+    print("Llave Privada:", llave_privada)
+    
+    publ = llave_a_texto_public(llave_publica)
+    priv = llave_a_texto_private(llave_privada)
+    print(publ)
+    print()
+    print(priv)
+    print()
+    print("Llave Pública:", texto_a_llave(publ))
+    print("Llave Privada:", texto_a_llave(priv))
+    
+    mensaje = "Hola mundo"
+    
+    mencifra = cifrar_mensaje(mensaje, llave_publica)
+    print(mencifra)    
+    print(f"Descifrado: {descifrar_mensaje(mencifra, llave_privada)}") """
+    
+    # Iniciando la aplicación
+    app = QtWidgets.QApplication([])
+    
+    # Cargar archivos .ui
+    inicio = uic.loadUi('inicio.ui')
+    generar = uic.loadUi('generar.ui')
+    encriptar = uic.loadUi('encriptar.ui')
+    desencriptar = uic.loadUi('desencriptar.ui')
+    
+    # Relacionando los Botones de Inicio
+    inicio.btnGenerar.clicked.connect(gui_generar)
+    inicio.btnEncriptar.clicked.connect(gui_encriptar)
+    inicio.btnDesencriptar.clicked.connect(gui_desencriptar)
+    
+    # Relacionando los Botones de Generar
+    generar.btnRegresar.clicked.connect(volver)
+    generar.btnGenerarPrimos.clicked.connect(btnGenerarPrimos)
+    generar.btnGenerarPublica.clicked.connect(btnGenerarPublica)
+    generar.btnGenerarPrivada.clicked.connect(btnGenerarPrivada)
+    
+    
+    # Relacionando los Botones de Encriptar
+    encriptar.btnRegresar.clicked.connect(volver)
+    
+    # Relacionando los Botones de Desencriptar
+    desencriptar.btnRegresar.clicked.connect(volver)
+    
+    # Ejecutable
+    inicio.show()
+    app.exec()
---
+++ package/utilidades.py
@@ -0,0 +1,127 @@
+import random
+from sympy import mod_inverse
+
+
+key = "AFOE439&(y"
+key2 = "91642ris#x"
+key3 = "83rqposgk0"
+
+def encrypt(value: int) -> str:
+    value_encypt = ""
+    
+    for number in str(value):
+        value_encypt += key[int(number)] + key2[int(number)] + key3[int(number)]
+        
+    return value_encypt
+
+def decrypt(value: str) -> int:
+    value_key = ""
+    for letter in value[::3]:
+        value_key += str(key.index(letter))
+        
+    return int(value_key)
+
+def generar_primo(bits) -> int:
+    # Generar un número primo aleatorio de 'bits' bits
+    primo = 4  # Inicialización para entrar al bucle
+    while not es_primo(primo):
+        primo = random.getrandbits(bits)
+    return primo
+
+def es_primo(n, k=5) -> bool:
+    # Probabilisticamente verifica si un número es primo usando el test de Miller-Rabin
+    if n <= 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+
+    # Expresa n - 1 como 2^r * d
+    r, d = 0, n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+
+    # Realiza el test de Miller-Rabin k veces
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+def generar_clave_privada(p: int, q:int, e:int = 65537) -> tuple:
+    N = p * q
+    phi_N = (p - 1) * (q - 1)
+
+    # Calcula el exponente de desencriptación 'd'
+    d = mod_inverse(e, phi_N)
+    # Llave privada
+    llave_privada = (N, d)
+    
+    return llave_privada
+
+def generar_clave_publica(p: int, q:int,  e:int = 65537) -> tuple:
+    N = p * q
+    # Llave pública
+    llave_publica = (N, e)
+    
+    return llave_publica
+
+def cifrar_mensaje(mensaje: str, llave_publica: tuple) -> str:
+    # Desempaquetar la llave pública
+    N, e = llave_publica
+    
+    # Convertir el mensaje a números (por ejemplo, usando codificación ASCII)
+    mensaje_numeros = [ord(c) for c in mensaje]
+
+    # Cifrar cada número del mensaje
+    mensaje_cifrado = ([pow(num, e, N) for num in mensaje_numeros])
+
+    return mensaje_cifrado
+
+def descifrar_mensaje(mensaje_cifrado: str, llave_privada: tuple) -> str:
+    # Desempaquetar la llave privada
+    N, d = llave_privada
+
+    # Descifrar cada número del mensaje cifrado
+    mensaje_descifrado_numeros = [pow(num, d, N) for num in mensaje_cifrado]
+
+    # Convertir los números descifrados a caracteres
+    mensaje_descifrado = ''.join([chr(num) for num in mensaje_descifrado_numeros])
+
+    return mensaje_descifrado
+
+def llave_a_texto_public(llave: tuple):
+    # Convierte una llave publica a un formato de texto plano
+    n, e = llave
+    texto_plano = f"\t==== LLAVE PUBLICA ====\n{encrypt(n)}\n{encrypt(e)}"
+    return texto_plano
+
+def llave_a_texto_private(llave: tuple):
+    # Convierte una llave privada a un formato de texto plano
+    n, d = llave
+    texto_plano = f"\t==== LLAVE PRIVADA ====\n{encrypt(n)}\n{encrypt(d)}"
+    return texto_plano
+
+def texto_a_llave(texto_plano: str):
+    # Parsea el formato de texto plano y recupera la llave
+    lines = texto_plano.split('\n')
+    n = int(decrypt(lines[1]))
+    exponent = int(decrypt(lines[2]))
+    llave = (n, exponent)
+    return llave
+
+def es_numero(number: str) -> bool:
+    try:
+        int(number)
+    except:
+        return False
+    return True
